5bebd7cea7a0bda19b3fed3632fb23288369d076
AngeloLista <abstractplayer@gmail.com>
2022-01-25 22:55:20 +0100

    created objects array

---
+++ js/scripts2.js
@@ -0,0 +1,17 @@
+const studente = [
+    {
+        nome: 'Vincenzo',
+        cognome: 'Rossi',
+        anni: 23,
+    },
+    {
+        nome: 'Marco',
+        cognome: 'Ferrari',
+        anni: 21,
+    },
+    {
+        nome: 'Francesco',
+        cognome: 'Costa',
+        anni: 25,
+    },
+];
